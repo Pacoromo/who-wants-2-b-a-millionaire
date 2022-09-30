5f1628e7ee24e28fdd5a58cc6c9fbd4a72c6c14a
pacoromo <pacoromo@me.com>
2022-09-30 10:04:53 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+//Who wants to be a millionaire
+
+A landing page with the app heading "Who wants to be a millionaire"
+
+A backrounbd related to the TV Show
+    -An Enter button
+
+A second page:
+    -Game rules description
+    -Ask for the player's name with a text input
+    -A start button
+
+ A third page with the game's board
+    -a section showing the current question
+    -a section with 4 buttons with every option
+    -a score section hightlighting the current price pool
+
+-print the question that we have obtained from The API
+-print the 4 possible answers as buttons
+-start listening for the user answer
+-start the timer
+-play a background sound
+-make a function to compare selection
+-play a sound when user makes a selection
+-if no selection is made and the timer runs out the -answer is considered as wrong
+-play a sound when answer is wrong or timer runs out
+-play a sound when answer is right
+
+    -If answer was right:
+        -increase the score
+        -Check if user won
+        If user won: show the results screen
+    -if answer was wrong
+        -Show the results screen */
