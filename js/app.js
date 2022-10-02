284a57c0526898e0b7cf636e9e2f8758a2511218
pacoromo <pacoromo@me.com>
2022-10-02 13:47:03 -0400

    modal markup

--- js/app.js
+++ js/app.js
@@ -10,6 +10,13 @@ app.toggleScreen = (element) => {
 
 //Show start screen
 
+/*
+A landing page with the app heading "Who wants to be a millionaire"
+
+A backrounbd related to the TV Show
+    -An Enter button
+*/
+
 app.startScreen = () => {
   const header = document.querySelector("header");
   const enterBtn = document.getElementById("enter-btn");
@@ -18,6 +25,7 @@ app.startScreen = () => {
     "click",
     () => {
       app.toggleScreen(header);
+      //Call next screen
       app.rulesScreen();
     },
     { once: true }
@@ -25,6 +33,11 @@ app.startScreen = () => {
 };
 
 //Show rules screen
+/*  
+A second page:
+    -Game rules description
+    -Ask for the player's name with a text input
+    -A start button*/
 
 app.rulesScreen = () => {
   const rulesSection = document.querySelector(".rules-screen");
@@ -35,7 +48,9 @@ app.rulesScreen = () => {
     "submit",
     (e) => {
       e.preventDefault();
+      app.playerName = document.getElementById("player-name").value; //namespace scope
       app.toggleScreen(rulesSection);
+      //Call next screen
       app.gameBoardScreen();
     },
     { once: true }
@@ -44,8 +59,39 @@ app.rulesScreen = () => {
 
 //Show gameboard screen
 
+/*
+A third page with the game's board
+-a section showing the current question
+-a section with 4 buttons with every option
+-a score section hightlighting the current price pool
+
+-print the question that we have obtained from The API
+-print the 4 possible answers as buttons
+-start listening for the user answer
+-start the timer
+-play a background sound
+
+-check if user wants to walk away
+if yes: check the next lowest threshold
+print the results
+
+-make a function to compare selection
+-play a sound when user makes a selection
+-if no selection is made and the timer runs out the -answer is considered as wrong
+-play a sound when answer is wrong or timer runs out
+-play a sound when answer is right
+
+-If answer was right:
+    -increase the score
+    -Check if user won
+    If user won: show the results screen
+-if answer was wrong
+    -Show the results screen */
+
 app.gameBoardScreen = () => {
   const gameBoardsection = document.querySelector(".game-board-screen");
+  const playerNameDisplay = document.querySelector(".player-name");
+  playerNameDisplay.textContent = app.playerName;
   app.toggleScreen(gameBoardsection);
 };
 
@@ -56,18 +102,8 @@ app.init = () => {
 
 app.init();
 
-/**
-//Who wants to be a millionaire
+/*
 
-A landing page with the app heading "Who wants to be a millionaire"
-
-A backrounbd related to the TV Show
-    -An Enter button
-
-A second page:
-    -Game rules description
-    -Ask for the player's name with a text input
-    -A start button
 
  A third page with the game's board
     -a section showing the current question
